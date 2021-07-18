a3ea0946672eeb41af5e22d7d80f7ecc5efafa8d
0xPiyush <piyush.sharma.bsp@gmail.com>
2021-07-18 21:08:33 +0530

    added prerendering

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
 <script>
 	import PokeCard from '../components/pokecard.svelte';
 	import { fetchPokemon, sort_by_key } from '../utils';
